d46165120b9b5e6e5d9b6c09bbb36e72c348f3a5
fujioka-a <atsushi3233@gmail.com>
2025-06-10 08:50:50 +0900

    .gitignoreにnode_modulesを追加し、package-lock.jsonとpyrightconfig.jsonを新規作成。config.pyで環境変数のデフォルト値を設定し、handler.pyを削除。main.pyでuser.routerのインクルードをコメントアウト。

--- src/core/config.py
+++ src/core/config.py
@@ -4,8 +4,8 @@ from functools import lru_cache
 
 class Settings:
     region: str = os.getenv("AWS_REGION", "ap-northeast-1")
-    user_pool_id: str = os.getenv("USER_POOL_ID")
-    app_client_id: str = os.getenv("APP_CLIENT_ID")
+    user_pool_id: str = os.getenv("USER_POOL_ID", "")
+    app_client_id: str = os.getenv("APP_CLIENT_ID", "")
 
 
 @lru_cache
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-from .routers import task, user
+from .routers import task
 
 app = FastAPI(title="Serverless FastAPI with Cognito")
 
-app.include_router(user.router)
+# app.include_router(user.router)
 app.include_router(task.router)
